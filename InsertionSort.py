ab713245cfdeb210643843ce7200501c34c5b302
Prajwal M <122041305+Prajwal-M-Rao@users.noreply.github.com>
2025-04-09 00:43:07 +0530

    Add files via upload

---
+++ InsertionSort.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        # Move elements greater than key one position ahead
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+# Example
+arr = [5, 3, 4, 1, 2]
+sorted_arr = insertion_sort(arr)
+print("Sorted array:", sorted_arr)
